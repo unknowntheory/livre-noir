35e0255ef4e86d1da4077426674da46d659c8944
alnano <alnano2@gmail.com>
2018-02-05 13:53:52 -0800

    completed tests

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { Grid, Image } from 'semantic-ui-react'
-import { gg } from './styles.css'
+import { gg } from '../Home/styles.css'
+// '../Home/styles.css'
 // const tt = require('../assets/image/imgplace.svg')
 // const gg = require('./tcc.png')// need image / url loaders
 class Home extends Component {
 
   render() {
     return (
-     <Grid centered columns={2}>
      <h1 className='gg'>test</h1>
-      <Grid.Column>
-          <Image src='https://i.imgur.com/xfJDww4.jpg' />
-      </Grid.Column>
-     </Grid>
+    //  <Grid centered columns={2}>
+    //   <Grid.Column>
+    //       <Image src='https://i.imgur.com/xfJDww4.jpg' />
+    //   </Grid.Column>
+    //  </Grid>
     );
   }
 }
