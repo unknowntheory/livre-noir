70eed765a21b4131b2d26744eb85dda9050f3254
alnano <alnano2@gmail.com>
2018-02-05 16:33:44 -0800

    button ui mockup completed

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Button } from 'semantic-ui-react'
 import { gg } from '../Home/styles.css'
 // '../Home/styles.css'
 // const tt = require('../assets/image/imgplace.svg')
@@ -8,12 +8,22 @@ class Home extends Component {
 
   render() {
     return (
-     <h1 className='gg'>test</h1>
-    //  <Grid centered columns={2}>
-    //   <Grid.Column>
-    //       <Image src='https://i.imgur.com/xfJDww4.jpg' />
-    //   </Grid.Column>
-    //  </Grid>
+      <div>
+     <Grid centered columns={2}>
+      <Grid.Column>
+          <Image src='https://i.imgur.com/xfJDww4.jpg' />
+
+      </Grid.Column>  
+     </Grid>
+     <Grid centered columns={5}>
+        <Grid.Column>
+          <Button.Group>
+            <Button labelPosition='left' icon='left chevron' content='Back' />
+            <Button labelPosition='right' icon='right chevron' content='Forward' />
+          </Button.Group>
+        </Grid.Column>
+     </Grid>
+     </div>
     );
   }
 }
