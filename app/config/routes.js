e2a786ab5ee827834821aa7c6843e85dc399b354
Daniel London <theryy408@gmail.com>
2018-02-13 19:46:46 -0800

    hard switch to Upload ft

--- app/config/routes.js
+++ app/config/routes.js
@@ -19,7 +19,7 @@ const routes = (
   <BrowserRouter>
     <MainContainer>
       <Switch>
-      <Route exact path='/' component={HomeContainer} />
+      <Route exact path='/' component={UploadContainer} />
       </Switch>
     </MainContainer>
   </BrowserRouter>
