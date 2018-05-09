88ce70ae6215add636d765a6a20e2bb00bbea41f
Daniel London <theryy408@gmail.com>
2018-05-08 20:55:42 -0700

    deleted old code that dealth with react dropzone

--- sev/index.js
+++ sev/index.js
@@ -15,51 +15,12 @@ const accessToken = process.env.API_AccessToken
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/upload', (req, res) =>{
+  let contents = req.body.information;
+  console.log(contents);
 
-  let data = req.body.information.file.split(',')[1]
-  console.log(data,'data');
-  axios.post('https://api.imgur.com/3/image',data,{
-    headers: {'Authorization' : `Bearer ${accessToken}`},
-    name:'test'
-  }).then((resp)=>{
-    let userData = req.body.information
-    userData['picture'] = resp.link
-    console.log(resp,'resp')
-    // save to db
-  }).catch((error)=>{
-    if (error.response) {
-      console.log(error.response.data,'data');
-      console.log(error.response.status,'status');
-      console.log(error.response.headers,'headers');
-    } else if (error.request) {
-      console.log(error.request,'request');
-    } else {
-      console.log('Error', error.message);
-    }
-  })
 })
 
 
-//
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/1337');
-//
-//
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   // we're connected!
-//   console.log('connected');
-// });
-//
-// var uploadSchema = mongoose.Schema({
-//   name: String,
-//   title: String,
-//   description: String,
-//   picture: String,
-//   date: { type: Date, default: Date.now },
-// });
-//
 
 
 
